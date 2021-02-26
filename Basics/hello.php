a8c904f1c452cdb3fd921d61bfb3942f0f1a81bf
ErikEspinoza <ee227@njit.edu>
2021-02-26 12:52:48 -0500

    Not config.php

---
+++ Basics/hello.php
@@ -0,0 +1,20 @@
+<?php
+echo "hello world";
+$message = "Hello";
+echo "<br>\n";
+echo "$ message";
+echo "My variable contains : $message";
+echo "<br>\n";
+echo 'My Variable contains: $message';
+echo 'My string \"double qoutes" ';
+echo "<br>\n";
+echo "My string " . $message;
+echo  "<br>\n";
+
+
+echo 1 + "3";
+
+echo "<br>\n";
+echo 1 . "3";
+
+
