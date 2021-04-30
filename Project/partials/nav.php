3f35860d74266ccff47768e20e2d9bd7dde6e35b
Erik Espinoza <ee227@afsconnect1.njit.edu>
2021-04-29 22:22:44 -0400

    New update for Table2

--- Project/partials/nav.php
+++ Project/partials/nav.php
@@ -1,6 +1,5 @@
 <link rel="stylesheet" href="static/css/styles.css">
 <?php
-//we'll be including this on most/all pages so it's a good place to include anything else we want on those pages
 require_once(__DIR__ . "/../lib/helpers.php");
 ?>
 <nav>
@@ -10,9 +9,16 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <li><a href="login.php">Login</a></li>
         <li><a href="register.php">Register</a></li>
     <?php endif; ?>
+    <?php if(HAS_ROLE("Admin")): ?>
+        <li><a href = "create_table_scores.php"> Create Score</a></li>
+        <li><a href = "test_list_scores.php"> List Score</a></li>
+        <li><a href = "test_create_scorehistory.php"> Create Score History </a></li>
+        <li><a href = "test_list_scorehistory.php"> List score History </a> </li>
+    <?php endif; ?>
     <?php if (is_logged_in()): ?>
         <li><a href="profile.php">Profile</a></li>
         <li><a href="logout.php">Logout</a></li>
+        <li><a href="pong.html">PlayGame</a></li>
     <?php endif; ?>
 </ul>
 </nav>
