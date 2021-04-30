8485d6ef2fa8e48ccd0b95eae4473612200cd3fa
Erik Espinoza <ee227@afsconnect1.njit.edu>
2021-04-29 22:12:26 -0400

    This is Table 2

--- Project/create_table_scores.php
+++ Project/create_table_scores.php
@@ -8,24 +8,6 @@ if (!has_role("Admin")) {
 ?>
 
  <form method="POST"> 
-<!--
-	<label>Name</label>
-	<input name="name" placeholder="Name"/>
-	<label>State</label>
-	<select name="state">
-		<option value="0">Incubating</option>
-		<option value="1">Hatching</option>
-		<option value="2">Hatched</option>
-		<option value="3">Expired</option>
-	</select>
-	<label>Base Rate</label>
-	<input type="number" min="1" name="base_rate"/>
-	<label>Mod Min</label>
-	<input type="number" min="1" name="mod_min"/>
-	<label>Mod Max</label>
-	<input type="number" min="1" name="mod_max"/>
-	<input type="submit" name="save" value="Create"/>
--->
 
 	<label>scores</label>
 	<input type="number" min="0" name="score"/>
@@ -34,18 +16,6 @@ if (!has_role("Admin")) {
 
 <?php
 if(isset($_POST["save"])){
-	//TODO add proper validation/checks
-	/*
-	$name = $_POST["name"];
-	$state = $_POST["state"];
-	$br = $_POST["base_rate"];
-	$min = $_POST["mod_min"];
-	$max = $_POST["mod_max"];
-	$nst = date('Y-m-d H:i:s');//calc
-	*/
-	//commented out the above
-	
-	//$id = get_id();
 	$user = get_user_id();
 	$score=$_POST["score"];
 	$db = getDB();
